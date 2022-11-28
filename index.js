c4f8b229cd9f804e7328478d3c27aa3ef2e6ea8c
OyakMasu21 <akzaki17@gmail.com>
2022-11-28 12:30:00 +0300

    Completed challenge 2

--- index.js
+++ index.js
@@ -38,20 +38,20 @@ else if(marks >=0 && marks <=100){
  
     }
     gradingSystem()
-
-//The function for the speed detector system
+//Had to define the variable    
 let speed;
+//The function for the speed detector system 
 function speedDetectorSystem(speed){
     if(speed < 70){
        return'Ok'
-     }else if(speed>70){
+//The  creation of the demerit points calculator       
+     }else if(speed>=70){
         let points = (speed-70)/5;
         console.log(points)
           if(points>12){
             console.log("License suspended") 
         }
-        
-
      }
-speedDetectorSystem()
 }
+
+
---
+++ speed-generator.js
@@ -0,0 +1,16 @@
+//Had to define the variable    
+let speed;
+//The function for the speed detector system 
+function speedDetectorSystem(speed){
+    if(speed < 70){
+       return'Ok'
+//The  creation of the demerit points calculator       
+     }else if(speed>=70){
+        let points = (speed-70)/5;
+        console.log(points)
+          if(points>12){
+            console.log("License suspended") 
+        }
+     }
+}
+
